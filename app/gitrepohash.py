035e223f0d81c3ca82f86daddb8a221d8c81c7bb
Son Truong <truongst@hotmail.com>
2020-07-12 21:34:22 +1000

    add unit test and web wrapper

--- app/gitrepohash.py
+++ app/gitrepohash.py
@@ -1,12 +1,14 @@
 import subprocess
 
-def get_git_version_hash():
+def get_git_repo_hash():
     hashes = []
+    hashes.append('version: ')
     hashes.append(subprocess.check_output(['git', 'tag']))
+    hashes.append('lastcommitsha: ')
     hashes.append(subprocess.check_output(['git', 'rev-parse', 'HEAD']))
-    hashes.append('"description" : "pre-interview technical test"')
+    hashes.append('description: pre-interview technical test')
     
     return hashes
 
 if __name__ == "__main__":
-    print(get_git_version_hash())
+    print(get_git_repo_hash())
---
+++ test/test_gitrepohash.py
@@ -0,0 +1,8 @@
+
+import unittest
+
+from app import gitrepohash
+
+def test_appexecution() :
+    result = gitrepohash
+    assert result
