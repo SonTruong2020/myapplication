482ed9e4d322a5514f005d2238941d77d2ec79ae
Son Truong <truongst@hotmail.com>
2020-07-17 10:27:57 +1000

    Completed

--- endpoint.py
+++ endpoint.py
@@ -2,7 +2,6 @@ import os
 import json
 
 from flask import Flask
-
 from app import gitrepohash
 
 app = Flask(__name__)
@@ -19,11 +18,12 @@ def endpoint():
 @app.route("/version")
 def endpoint1():
  #   page = gitrepohash.get_git_repo_hash()
-    page = '<html><body><h1>'
-    page += json.dumps(gitrepohash.get_git_repo_hash(),indent=4)
-    page += '<h1></body></html>'
+ #   page = '<html><body><h1>'
+ #   page += json.dumps(gitrepohash.get_git_repo_hash(),indent=4)
+ #   page += '<h1></body></html>'
+    githash =  gitrepohash.get_git_repo_hash()
 
-    return page
+    return json.dumps(githash,indent=4)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
